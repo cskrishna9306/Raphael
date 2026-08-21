13e92001de917f6aa04f4f4210043798da5dfca1
Sai Chaparala <79293787+cskrishna9306@users.noreply.github.com>
2026-08-20 22:37:18 -0700

    use relative package imports

--- src/raphael/agentry/models/__init__.py
+++ src/raphael/agentry/models/__init__.py
@@ -1,9 +1,9 @@
 # Import the sub-modules leaf-first to avoid circular dependencies
-from src.raphael.agentry.models.Gender import Gender
-from src.raphael.agentry.models.RolePresence import RolePresence
-from src.raphael.agentry.models.CharacterProfile import CharacterProfile
-from src.raphael.agentry.models.Cast import Cast
-from src.raphael.agentry.models.Screenplay import Screenplay
+from .Gender import Gender
+from .RolePresence import RolePresence
+from .CharacterProfile import CharacterProfile
+from .Cast import Cast
+from .Screenplay import Screenplay
 
 __all__ = [
     "Screenplay",
